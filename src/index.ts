3fee43367193a14c8f791c2d94ae8f82b1401c53
Gustavo Barbosa <gusbdev@gmail.com>
2024-10-09 23:21:42 -0300

    feat: export EventProvider, useCaptureEvent, and CapturedEventsList components

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { EventProvider, useCaptureEvent } from "./context/CaptureEventProvider";
+export { CapturedEventsList } from "./components/CapturedEventsList";
+
+// @ts-ignore
+export { registerServiceWorker } from "./sw/service-worker.js";
