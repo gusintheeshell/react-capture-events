638c6fda004a14654c84b796a65c6a537f76327d
Gustavo Barbosa <gusbdev@gmail.com>
2024-10-12 14:15:31 -0300

    feat: remove unnecessary files and code

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export { EventProvider, useCaptureEvent } from "./context/CaptureEventProvider";
-export { CapturedEventsList } from "./components/CapturedEventsList";
-
-// @ts-ignore
-export { registerServiceWorker } from "./sw/service-worker.js";
