e9e02cf485fdee995d93e6d3cd8b2773005162bd
Gustavo Barbosa <gusbdev@gmail.com>
2024-10-12 16:35:30 -0300

    refactor: adjust import paths and remove unnecessary semicolons in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
@@ -8,6 +8,14 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ['dist', 'lib', 'node_modules', 'coverage', 'public', 'build'],
+    ignores: [
+      'dist',
+      'lib',
+      'node_modules',
+      'coverage',
+      'public',
+      'build',
+      'tests',
+    ],
   },
-];
+]
