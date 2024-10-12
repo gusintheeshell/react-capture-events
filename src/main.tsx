0d0468fb2813ee18dba081a360d5d3eccaf062ac
Gustavo Barbosa <gusbdev@gmail.com>
2024-10-12 14:17:08 -0300

    refactor: adjust import paths and add CapturedEventsList component

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./App.css";
 
-import { EventProvider, registerServiceWorker } from "./index.ts";
+import { EventProvider, registerServiceWorker } from "../lib/main.ts";
 
 registerServiceWorker();
 
