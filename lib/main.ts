b1200af1351894c7b6bbfedcdbd3a0b00f52d9fd
Gustavo Barbosa <gusbdev@gmail.com>
2024-10-12 14:12:46 -0300

    feat: add CaptureEventProvider and CapturedEventsList components

---
+++ lib/main.ts
@@ -0,0 +1,5 @@
+export * from "./context/CaptureEventProvider";
+import { CapturedEventsList } from "./components/CapturedEventsList";
+import { registerServiceWorker } from "./utils";
+
+export { CapturedEventsList, registerServiceWorker };
