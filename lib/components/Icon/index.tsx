d32610dbb87a47ee98b6fa7680195f07c6fb1c55
Gustavo Barbosa <gusbdev@gmail.com>
2024-10-14 11:23:48 -0300

    refactor: update Icon component to use an image instead of SVG

--- lib/components/Icon/index.tsx
+++ lib/components/Icon/index.tsx
@@ -1,58 +1,13 @@
 import React from 'react'
 
-const Icon: React.FC = () => (
-  <svg
-    width="200"
-    height="200"
-    viewBox="0 0 200 200"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <circle
-      cx="100"
-      cy="100"
-      r="90"
-      stroke="#61DAFB"
-      strokeWidth="10"
-      strokeDasharray="20 10"
-    />
-
-    <g transform="translate(100 100)">
-      <ellipse
-        rx="45"
-        ry="15"
-        stroke="#61DAFB"
-        strokeWidth="10"
-        transform="rotate(0)"
-      />
-      <ellipse
-        rx="45"
-        ry="15"
-        stroke="#61DAFB"
-        strokeWidth="10"
-        transform="rotate(60)"
-      />
-      <ellipse
-        rx="45"
-        ry="15"
-        stroke="#61DAFB"
-        strokeWidth="10"
-        transform="rotate(120)"
-      />
-
-      <circle r="15" fill="#61DAFB" />
-    </g>
-
-    <circle
-      cx="100"
-      cy="100"
-      r="30"
-      stroke="#FF4154"
-      strokeWidth="10"
-      fill="none"
-    />
-    <circle cx="100" cy="100" r="10" fill="#FF4154" />
-  </svg>
-)
+export type IconProps = {
+  src: string
+  alt: string
+  width: number
+  height: number
+}
+const Icon: React.FC<IconProps> = ({ src, alt, width, height }) => {
+  return <img src={src} alt={alt} width={width} height={height} />
+}
 
 export default Icon
