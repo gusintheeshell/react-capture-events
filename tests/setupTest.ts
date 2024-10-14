02aaf23f474ba2981a1500ae1131676dc25d5e8c
Gustavo Barbosa <gusbdev@gmail.com>
2024-10-14 12:03:46 -0300

    refactor: update setupTest.ts to include global variable for React Act environment

--- tests/setupTest.ts
+++ tests/setupTest.ts
@@ -3,6 +3,27 @@ import { afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
 
+declare global {
+  interface Window {
+    IS_REACT_ACT_ENVIRONMENT?: boolean
+  }
+}
+
+Object.defineProperty(globalThis, 'IS_REACT_ACT_ENVIRONMENT', {
+  get() {
+    if (typeof globalThis.self !== 'undefined') {
+      return (globalThis.self as any)._IS_REACT_ACT_ENVIRONMENT
+    }
+  },
+  set(value) {
+    if (typeof globalThis.self !== 'undefined') {
+      ;(globalThis.self as any)._IS_REACT_ACT_ENVIRONMENT = value
+    }
+  },
+})
+
+globalThis.IS_REACT_ACT_ENVIRONMENT = true
+
 afterEach(() => {
   cleanup()
 })
