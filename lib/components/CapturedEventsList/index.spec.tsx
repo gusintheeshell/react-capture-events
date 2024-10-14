858b354510eae0afaa484be55fc28be84aecdb2e
Gustavo Barbosa <gusbdev@gmail.com>
2024-10-14 12:07:40 -0300

    refactor(test): using act to avoid warning

--- lib/components/CapturedEventsList/index.spec.tsx
+++ lib/components/CapturedEventsList/index.spec.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom'
 import { describe, expect, it } from 'vitest'
 import userEvent from '@testing-library/user-event'
-import { render, waitFor, screen } from '@testing-library/react'
+import { render, waitFor, screen, act } from '@testing-library/react'
 import { CapturedEventsList } from './index'
 
 describe('CapturedEventsList Component', () => {
@@ -15,7 +15,10 @@ describe('CapturedEventsList Component', () => {
   it('should open and close the debug window', async () => {
     render(<CapturedEventsList />)
     const toggleButton = screen.getByTestId('toggle-button')
-    userEvent.click(toggleButton)
+
+    await act(async () => {
+      userEvent.click(toggleButton)
+    })
 
     const debugWindow = await waitFor(() => {
       const element = screen.getByTestId('debug-window')
@@ -24,7 +27,10 @@ describe('CapturedEventsList Component', () => {
     })
 
     expect(debugWindow).toBeVisible()
-    userEvent.click(toggleButton)
+
+    await act(async () => {
+      userEvent.click(toggleButton)
+    })
 
     await waitFor(() => {
       expect(debugWindow).not.toBeVisible()
@@ -34,16 +40,25 @@ describe('CapturedEventsList Component', () => {
   it('should switch between individual and table view', async () => {
     render(<CapturedEventsList />)
     const toggleButton = screen.getByTestId('toggle-button')
-    userEvent.click(toggleButton)
+
+    await act(async () => {
+      userEvent.click(toggleButton)
+    })
 
     const toggleViewButton = await waitFor(() =>
       screen.getByTestId('toggle-view-button'),
     )
-    userEvent.click(toggleViewButton)
+
+    await act(async () => {
+      userEvent.click(toggleViewButton)
+    })
 
     const tableView = await waitFor(() => screen.getByTestId('table-view'))
     expect(tableView).toBeVisible()
-    userEvent.click(toggleViewButton)
+
+    await act(async () => {
+      userEvent.click(toggleViewButton)
+    })
 
     const individualView = await waitFor(() =>
       screen.getByTestId('individual-view'),
@@ -54,10 +69,16 @@ describe('CapturedEventsList Component', () => {
   it('should clear all events', async () => {
     render(<CapturedEventsList />)
     const toggleButton = screen.getByTestId('toggle-button')
-    userEvent.click(toggleButton)
+
+    await act(async () => {
+      userEvent.click(toggleButton)
+    })
 
     const clearButton = await waitFor(() => screen.getByTestId('clear-button'))
-    userEvent.click(clearButton)
+
+    await act(async () => {
+      userEvent.click(clearButton)
+    })
 
     const noEventsText = await waitFor(() => screen.getByText('No events yet'))
     expect(noEventsText).toBeInTheDocument()
